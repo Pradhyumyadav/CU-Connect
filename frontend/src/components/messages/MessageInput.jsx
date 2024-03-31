1e19882d8dc6a9cdf0b6621e1c5d1b3dcd10762d
Pradhyumyadav <81881713+Pradhyumyadav@users.noreply.github.com>
2024-03-31 12:46:43 +0100

    .

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BsSend } from "react-icons/bs";
-import useSendMessage from "../../hooks/useSendMessage";
+import useSendMessage from "../../hooks/useSendMessage.js";
 
 const MessageInput = () => {
 	const [message, setMessage] = useState("");

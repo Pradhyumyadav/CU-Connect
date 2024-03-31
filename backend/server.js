a83135cf5a42a3d4f46b68f30b5485a682161be8
Pradhyumyadav <81881713+Pradhyumyadav@users.noreply.github.com>
2024-03-31 12:18:40 +0100

    Socket.io implemented

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,35 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRoutes from  "./routes/auth.routes.js";
-import messageRoutes from  "./routes/message.routes.js";
-import userRoutes from  "./routes/user.routes.js";
 
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
-import connectToMongodb from "./db/connectToMongodb.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 dotenv.config();
 
-app.use(express.json()); // Middleware for parsing JSON bodies (from req.body)
-app.use(cookieParser()); // Middleware
+app.use(express.json()); // to parse the incoming requests with JSON payloads
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// Connect to MongoDB database
-app.listen(PORT, () => {
-    connectToMongodb();
-    console.log(`server running on port ${PORT}`);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
